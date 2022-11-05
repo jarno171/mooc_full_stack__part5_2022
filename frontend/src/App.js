a45bc1d38a89b2a47f3e04146a34fb2001d532ad
Jarno Varis <jarno.varis@gmail.com>
2022-11-05 11:47:04 +0200

    now it shows the logged in user, and shows blogs if user is logged in, and also shows the logged users name

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -35,8 +35,9 @@ const App = () => {
     }
   }
 
-  return (
-    <div>
+  const loginForm = () => {
+    return (
+      <div>
 
       <form onSubmit={handleLogin}>
         <div>
@@ -61,11 +62,29 @@ const App = () => {
         <button type="submit">login</button>
       </form>
 
-      {/*
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}*/}
+    </div>
+    )
+  }
+
+  const blogList = () => {
+    return (
+      <div>
+        <h2>blogs</h2>
+        <p>{user.name} logged in</p>
+
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      { user === null ?
+        loginForm() :
+        blogList()
+      }
     </div>
   )
 }
