93792724a355f45abe35fcfb07447384a545fbaa
Jarno Varis <jarno.varis@gmail.com>
2022-11-07 18:07:22 +0200

    now blogs have open / close button, to change visibility of additional info

--- frontend/src/components/Blog.js
+++ frontend/src/components/Blog.js
@@ -1,7 +1,40 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({blog}) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <>
+      <div style={blogStyle}>
+        <div>
+          <p>{blog.title}</p>
+          <button style={hideWhenVisible} onClick={toggleVisibility}>view</button>
+        </div>
+        
+        <div style={showWhenVisible}>
+          <p>{blog.url}</p>
+          <p>likes {blog.likes} <button>like</button></p>
+          <p>{blog.author}</p>
+          <button onClick={toggleVisibility}>close</button>
+        </div>
+      </div> 
+    </> 
+  )
+}
 
 export default Blog
