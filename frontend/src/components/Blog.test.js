7532e534bde2a86e8ca8a67f4005dc54da246f04
Jarno Varis <jarno.varis@gmail.com>
2022-11-10 17:57:25 +0200

    tests now visibility after button is pressed

--- frontend/src/components/Blog.test.js
+++ frontend/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
@@ -9,19 +10,42 @@ describe('<Blog />', () => {
     const blog = {
       title: 'testiblogi',
       author: 'testaaja',
-      ulr: 'www.testi.com',
+      url: 'www.testi.com',
       likes: 30
     }
 
     render(<Blog blog={blog} />)
 
+    screen.debug()
+
     screen.getByText('testiblogi')
     screen.getByText('testaaja')
 
     const urlVisible = screen.queryByText('www.testi.com')
-    expect(urlVisible).toBeNull()
+    expect(urlVisible).not.toBeVisible()
 
-    const likesVisible = screen.queryByText('30')
-    expect(likesVisible).toBeNull()
+    const likesVisible = screen.queryByText('likes 30')
+    expect(likesVisible).not.toBeVisible()
+  })
+
+  test('clicking the view button shows url and likes of the blog', async () => {
+    const blog = {
+      title: 'testiblogi',
+      author: 'testaaja',
+      url: 'www.testi.com',
+      likes: 30
+    }
+
+    render(<Blog blog={blog} />)
+
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const urlVisible = screen.getByText('www.testi.com')
+    expect(urlVisible).toBeVisible()
+
+    const likesVisible = screen.getByText('likes 30')
+    expect(likesVisible).toBeVisible()
   })
 })
