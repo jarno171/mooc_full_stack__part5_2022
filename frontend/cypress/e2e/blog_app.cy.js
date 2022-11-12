46c9214bd08e1c55c0c1391737e045c4a5036e67
Jarno Varis <jarno.varis@gmail.com>
2022-11-12 10:08:24 +0200

    added test for wrong login, and checking the color of the notification

--- frontend/cypress/e2e/blog_app.cy.js
+++ frontend/cypress/e2e/blog_app.cy.js
@@ -6,6 +6,7 @@ describe('Blog app', function() {
       username: 'testinen',
       password: '123321'
     }
+
     cy.request('POST', 'http://localhost:3003/api/users/', user) 
     cy.visit('http://localhost:3000')
   })
@@ -16,9 +17,22 @@ describe('Blog app', function() {
     cy.contains('blogs').should('not.exist')
   })
 
-  it('login form can be opened', function() {
-    cy.get('input:first').type('testinen')
-    cy.get('input:last').type('123321')
-    cy.contains('login').click()
+  describe('login', () => {
+    it('wrong creds dont work', function() {
+      cy.get('input:first').type('aaa')
+      cy.get('input:last').type('bee')
+      cy.contains('login').click()
+      cy.contains('blogs').should('not.exist')
+      cy.contains('Wrong credentials')
+        .should('have.attr', 'style')
+        .then(style => expect(style).contains('color: red;'))
+    })
+
+    it('correct credentials work', function() {
+      cy.get('input:first').type('testinen')
+      cy.get('input:last').type('123321')
+      cy.contains('login').click()
+      cy.contains('blogs')
+    })
   })
 })
